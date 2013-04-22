0b45a32ac2892ffc9246f9f21d8ec0acbff6b4e0
norilicht <tugend.licht@gmail.com>
2013-04-22 11:36:58 +0900

    タグ検索スクリプトを追加

---
+++ bin/ec2-tag-search.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TAG_NAME=${1:-NONE}
+
+if [ $TAG_NAME = "NONE" ]; then
+        echo "Usage: $0 [tag_name]"
+        exit 1
+fi
+
+. /etc/sysconfig/aws.env
+
+ec2-describe-instances | grep $INSTANCE_ID | grep TAG | grep $TAG_NAME | cut -f5
