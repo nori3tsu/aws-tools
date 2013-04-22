e9a22ddd32d05d7d54cd7e01ad6c0844457847e5
norilicht <tugend.licht@gmail.com>
2013-04-22 13:10:34 +0900

    UTF-8に変更

--- install-service.sh
+++ install-service.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:/bin:/sbin
 
 SERVICE_NAME=$1
 if [ ! -f $ROOT_DIR/service/$SERVICE_NAME ]; then
-        echo "�T�[�r�X$1�͑��݂��܂���B"
+        echo "サービス$1は存在しません。"
         exit 1
 fi
 
