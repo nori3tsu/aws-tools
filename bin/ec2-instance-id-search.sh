4465910fb8c863ad9e6377f5fafc25a37fb25d65
norilicht <tugend.licht@gmail.com>
2013-04-23 14:24:49 +0900

    インスタンスID検索機能を追加

---
+++ bin/ec2-instance-id-search.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INSTANCE_NAME=${1:-NONE}
+
+if [ $INSTANCE_NAME = "NONE" ]; then
+        echo "Usage: $0 [instance_name]"
+        exit 1
+fi
+
+. /etc/sysconfig/aws.env
+
+ec2-describe-instances | grep TAG | grep $INSTANCE_NAME | cut -f3
