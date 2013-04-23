bad9b99805ffe9268ead93e4e5a480f4776b9ba1
root <root@ip-10-132-136-147.ap-northeast-1.compute.internal>
2013-04-23 13:02:35 +0000

    初期処理追加

--- install-service.sh
+++ install-service.sh
@@ -16,10 +16,6 @@ if [ ! -f $AWS_TOOLS_CONF ]; then
         cp $ROOT_DIR/config/aws.env $AWS_TOOLS_CONF
 fi
 
-echo "create file: /etc/init.d/$SERVICE_NAME"
-cp $ROOT_DIR/service/$SERVICE_NAME /etc/init.d/.
-chmod u+x /etc/init.d/$SERVICE_NAME
-
 echo "add service: $SERVICE_NAME"
 chkconfig --add $SERVICE_NAME
 chkconfig $SERVICE_NAME on
